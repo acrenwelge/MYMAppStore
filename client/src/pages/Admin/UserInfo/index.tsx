f02f17f080c924c1c0943ca48a89dd041c2f5632
Ncc <luminousncc@gmail.com>
2023-03-24 12:58:30 -0500

    admin get all user's data

--- client/src/pages/Admin/UserInfo/index.tsx
+++ client/src/pages/Admin/UserInfo/index.tsx
@@ -1,7 +1,19 @@
-import React, { useContext, useEffect, useState, useCallback } from "react";
-import { Header, Divider, Form, Table, Label, Menu, Icon, Grid,GridColumn,Container,Pagination} from "semantic-ui-react";
+import React, {useContext, useEffect, useState, useCallback} from "react";
+import {
+    Header,
+    Divider,
+    Form,
+    Table,
+    Label,
+    Menu,
+    Icon,
+    Grid,
+    GridColumn,
+    Container,
+    Pagination
+} from "semantic-ui-react";
 import * as libphonenumber from "libphonenumber-js";
-import { ApplicationContext } from "../../../context";
+import {ApplicationContext} from "../../../context";
 import Page from "../../../components/Page";
 import PropTypes from 'prop-types';
 import axios from "axios";
@@ -30,104 +42,62 @@ import AdminMenu from "../../../components/AdminMenu";
 //     // name: PropTypes.string.isRequired,
 //     // email: PropTypes.string.isRequired
 // };
+
+interface UserData {
+    user_id: number;
+    email: string;
+    name: string;
+}
+
 const AdminUserInfo: React.FC = (props): JSX.Element => {
-	const ctx = useContext(ApplicationContext);
-	const [name, setName] = useState<string>(ctx.user?.name ?? "");
-	const userName = ctx.user?.name;
-	const userEmail = ctx.user?.email;
-
-    const APIData = [{"name":"ncc","email":"ncc@me.com"},
-                {"name":"yushuang","email":"yushuang@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"3","email":"3@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"yasskin","email":"yasskin@me.com"},
-                {"name":"3","email":"3@me.com"},
-                {"name":"3","email":"3@me.com"}
-            
-            ];
-
-	return (
-        
-		<Container className="container-fluid" fluid style={{ padding: "2" }}>
-        <Grid columns={2}>
-			<Grid.Row>
-				<GridColumn width={3}>
-                <AdminMenu />
-				</GridColumn>
-
-				<GridColumn  width={12}>
-                <Table celled>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>User Name</Table.HeaderCell>
-        <Table.HeaderCell>Email</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+    const ctx = useContext(ApplicationContext);
+    const [name, setName] = useState<string>(ctx.user?.name ?? "");
+    const userName = ctx.user?.name;
+    const userEmail = ctx.user?.email;
 
 
-    <Table.Body>
-      {APIData.map((data) => {
-     return (
-        <>
-       <Table.Row>
-          <Table.Cell >{data.name}</Table.Cell>
-           <Table.Cell>{data.email}</Table.Cell>
-        </Table.Row>
-        </>
-   )})}
-</Table.Body>
+    const [userData, setUserData] = useState<UserData[]>([]);
 
-    <Table.Footer>
+    useEffect(() => {
+        fetch('/api/admin/user/data')
+            .then(response => response.json())
+            .then(data => setUserData(data))
+            .catch(error => console.error(error));
+    }, []);
 
-      <Table.Row>
-      <Table.HeaderCell colSpan='3'>
-      <Pagination
-    boundaryRange={0}
-    defaultActivePage={1}
-    ellipsisItem={10}
-    firstItem={null}
-    lastItem={null}
-    siblingRange={1}
-    totalPages={10}
-  />
-  </Table.HeaderCell>
-        {/* <Table.HeaderCell colSpan='3'>
-          <Menu floated='right' pagination>
-            <Menu.Item as='a' icon>
-              <Icon name='chevron left' />
-            </Menu.Item>
-            <Menu.Item as='a'>1</Menu.Item>
-            <Menu.Item as='a'>2</Menu.Item>
-            <Menu.Item as='a'>3</Menu.Item>
-            <Menu.Item as='a'>4</Menu.Item>
-            <Menu.Item as='a' icon>
-              <Icon name='chevron right' />
-            </Menu.Item>
-          </Menu>
-        </Table.HeaderCell> */}
-      </Table.Row>
-    </Table.Footer>
-                </Table>
-				</GridColumn>
-			</Grid.Row>
-		</Grid>
-		</Container>
-	);
+
+
+    return (
+
+        <Container className="container-fluid" fluid style={{padding: "2"}}>
+            <Grid columns={2}>
+                <Grid.Row>
+                    <GridColumn width={3}>
+                        <AdminMenu/>
+                    </GridColumn>
+
+                    <GridColumn width={12}>
+                        <Table>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell>Email</Table.HeaderCell>
+                                    <Table.HeaderCell>Username</Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>
+                            <Table.Body>
+                                {userData.map(user => (
+                                    <Table.Row key={user.user_id}>
+                                        <Table.Cell>{user.email}</Table.Cell>
+                                        <Table.Cell>{user.name}</Table.Cell>
+                                    </Table.Row>
+                                ))}
+                            </Table.Body>
+                        </Table>
+                    </GridColumn>
+                </Grid.Row>
+            </Grid>
+        </Container>
+    );
 };
 
 export default AdminUserInfo;
--- server/src/admin/admin.controller.ts
+++ server/src/admin/admin.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
+import {UserService} from "../user/user.service";
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+    constructor(private readonly userService:UserService) {}
+
+    @Get("user/data")
+    findAllUserg() {
+        return this.userService.findAll();
+    }
+
+}
