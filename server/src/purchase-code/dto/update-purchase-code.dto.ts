626447aca266d7e6dbd58567abb8496b717c6456
Zhiting Zhao <helenzhao@tamu.edu>
2023-03-30 22:51:42 -0500

    without purchase-code

--- server/src/purchase-code/dto/update-purchase-code.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePurchaseCodeDto } from './create-purchase-code.dto';
-
-export class UpdatePurchaseCodeDto extends PartialType(CreatePurchaseCodeDto) {}
