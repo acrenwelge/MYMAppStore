fe35a92c767a11654b221c0719f3b3cb96cb278c
GHysmu <yus60401@gmail.com>
2023-03-31 01:47:04 -0500

    refine login test

--- client/src/test/features/Login.feature
+++ client/src/test/features/Login.feature
@@ -4,31 +4,20 @@ So that I can manage my subscription of textbooks
 I want to use my genreal account to login
 
 Scenario: Successful Login with Valid entries
-    Given user navigates to the website lo login
+    Given user navigates to the website to login
     When I type 'yushuang@me.com' and 'yushuang' as email and password
     And I click on 'login'
     Then login must be successful.
 
 Scenario: Unsuccessful Login with Invalid Password
-    Given the user navigates to the website to login
-    When the user enters their email 'yushuang@me.com' and an invalid password, such as 'yushuang123'
-    And the user clicks on the 'login' button
-    Then the system should display an error message indicating that the login was unsuccessful due to an invalid password.
+    Given user navigates to the website to login
+    When I type 'yushuang@me.com' and 'yushuangsdede' as email and password
+    And I click on 'login'
+    Then login failed due to invalid password.
 
 Scenario: Unsuccessful Login with Invalid Email
 
-    Given the user navigates to the website to login
-    When the user enters an invalid email and a valid password
-    And the user clicks on the 'login' button
-    Then the system should display an error message indicating that the login was unsuccessful due to an invalid email
-
-Scenario: Unsuccessful Login with Empty Fields
-
-Given the user navigates to the website to login
-When the user leaves the email and/or password field empty
-And the user clicks on the 'login' button
-Then the system should display an error message indicating that the login was unsuccessful due to empty fields.
-
-
-
-
+    Given user navigates to the website to login
+    When I type 'yushuasdeng@me.com' and 'yushuang' as email and password
+    And I click on 'login'
+    Then login failed due to invalid email.
--- client/src/test/features/step-definitions/Login.js
+++ client/src/test/features/step-definitions/Login.js
@@ -15,58 +15,52 @@ let driver;
 
 // Set up Before and After hooks
 Before(function (env) {
-	driver = new webdriver.Builder().forBrowser("chrome").build();
+ driver = new webdriver.Builder().forBrowser("chrome").build();
 });
 
 After(function () {
-	driver.quit();
+ driver.quit();
 });
 
 //场景
-Given("user navigates to the website lo login", async () => {
-	await driver.get("http://localhost:3000/login");
-	await driver.sleep(3 * 1000);
-	//setTimeout(myFunction, 10000);
+Given("user navigates to the website to login", async () => {
+ await driver.get("http://localhost:3000/login");
+ await driver.sleep(3 * 1000);
+ //setTimeout(myFunction, 10000);
 });
 
-When("I type 'yushuang@me.com' and 'yushuang' as email and password", async () => {
+When('I type {string} and {string} as email and password', async (email,password) => {
 //When("the user enters their email and password", async () => {
-    driver.findElement(webdriver.By.id("email")).sendKeys("yushuang@me.com");
-    driver.findElement(webdriver.By.id("password")).sendKeys("yushuang");
-	await driver.sleep(6 * 1000);
+    driver.findElement(webdriver.By.id("email")).sendKeys(email);
+    driver.findElement(webdriver.By.id("password")).sendKeys(password);
+ await driver.sleep(6 * 1000);
 });
 
 When("I click on 'login'", async () => {
     await driver.findElement(webdriver.By.xpath(`//*[@id="root"]/div/main/div/form/button`)).click();
-	await driver.sleep(6 * 1000);
+ await driver.sleep(6 * 1000);
 });
 
 // Scenario 1: Successful Login with Valid Entries
 Then("login must be successful.", async () => {
-	let curURL =  await driver.getCurrentUrl();
+ let curURL =  await driver.getCurrentUrl();
     expect(curURL).to.equal("http://localhost:3000/");
 
-	//console.log(curURL);
+ //console.log(curURL);
     //expect(curURL).to.equal("http://localhost:3000/");
 });
 
-//Scenario 2: Unsuccessful Login with Invalid Password
-Then("the system should display an error message indicating that the login was unsuccessful due to an invalid password", async () => {
-	let errorMsg = await driver.findElement(webdriver.By.className("error-message")).getText();
-	expect(errorMsg).to.equal("Invalid password");
+//Scenario 2: Unsuccessful Login with Invalid Passwordcd
+Then("login failed due to invalid password.", async () => {
+ let errorMsg = await driver.findElement(webdriver.By.className("ui error message")).getText();
+ expect(errorMsg).to.equal("Unable to login. Please ensure your email and password are correct and try again.");
 });
 
 //Scenario 3: Unsuccessful Login with Invalid email
 //Pages -》 Login ——》 LocalLoginForm ---
 //js Find element , 找到页面上面的元素
-Then("the system should display an error message indicating that the login was unsuccessful due to an invalid email", async () => {
-	let errorMsg = await driver.findElement(webdriver.By.className("error-message")).getText();
-	expect(errorMsg).to.equal("Invalid email");
+Then("login failed due to invalid email.", async () => {
+ let errorMsg = await driver.findElement(webdriver.By.className("ui error message")).getText();
+ expect(errorMsg).to.equal("Unable to login. Please ensure your email and password are correct and try again.");
 });
 
-//Scenario 4： Unsuccessful Login with Empty Fields
-//前端的error message 是不是一样的？？
-Then("the system should display an error message indicating that the login was unsuccessful due to empty fields", async () => {
-	let errorMsg = await driver.findElement(webdriver.By.className("error-message")).getText();
-	expect(errorMsg).to.equal("Please fill in all fields");
-});
