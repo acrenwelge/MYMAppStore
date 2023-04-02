ad28fd2bd6b7436f0c89ae592d6cd41b6d2e9bcc
shuyuwang21 <112589309+shuyuwang21@users.noreply.github.com>
2023-04-01 23:35:48 -0500

    check adding purchase code

--- client/src/test/features/addPurchaseCode.feature
+++ client/src/test/features/addPurchaseCode.feature
@@ -3,14 +3,10 @@ As the seller
 So that I can provide create / discount code to specific customer 
 I want to offer some specific customers a discount when they pay for the items.
 
-Scenario: Successfully Accessing Purchase Code
-    Given user is logged in and at admin page
-    When clicking on purchase code page button
-    Then the user should be at purchase code page and see all purchase code
-
-Scenario: Accessing purchase code not successful
-    Given user is logged in and at home page
-    Then the user should not see admin button
-
-
+Scenario: Adding purchase code correctly
+    Given user is logged in as admin 
+    When go to purchase code page url
+    When type in valid purchase code
+	When click the add button
+    Then the purchase code should be added in
 
--- client/src/test/features/step-definitions/addPurchaseCode.js
+++ client/src/test/features/step-definitions/addPurchaseCode.js
@@ -15,54 +15,116 @@ After(function () {
 	driver.quit();
 });
 
-//Scenario: Successfully Accessing Purchase Code
-//    Given user is logged in and at admin page
-//    When clicking on purchase code page button
-//    Then the user should be at purchase code page and see all purchase code
+/*
+Scenario: Adding purchase code correctly
+    Given user is logged in as admin 
+    When go to purchase code page url
+    When type in valid purchase code
+	When click the add button
+    Then the purchase code should be added in
+*/
 
-Given("user is logged in and at admin page", async () => {
+Given("user is logged in as admin", async () => {
 	//log in: 
 	await driver.get("http://localhost:3000/login");
  	await driver.sleep(3 * 1000);
+
 	driver.findElement(webdriver.By.id("email")).sendKeys('ncc@me.com');
 	driver.findElement(webdriver.By.id("password")).sendKeys('ncc');
 	await driver.sleep(6 * 1000);
+	
 	await driver.findElement(webdriver.By.xpath(`//*[@id="root"]/div/main/div/form/button`)).click();
  	await driver.sleep(6 * 1000);
+	
 	//navigate to admin:
 	await driver.get("http://localhost:3000/admin");
 	await driver.sleep(3 * 1000);
 	//setTimeout(myFunction, 10000);
 });
-When("clicking on purchase code page button", async () => {
+
+When("go to purchase code page url", async () => {
     //driver.findElement(webdriver.By.id("email")).sendKeys("yushuang@me.com");
     //driver.findElement(webdriver.By.id("password")).sendKeys("yushuang");
 	//await driver.sleep(6 * 1000);
-	await driver.findElement(webdriver.By.xpath(`//*[@id="root"]/div/main/div/div/div/div[1]/div/div/a[4]`)).click();
-	///html/body/div/div/main/div/div/div/div[1]/div/div/a[4]
-	await driver.sleep(6 * 1000);
+	await driver.get("http://localhost:3000/admin/purchase-code");
+    await driver.sleep(3 * 1000);
+	
 });
-Then("the user should be at purchase code page and see all purchase code", async () => {
-	let curURL =  await driver.getCurrentUrl();
-    expect(curURL).to.equal("http://localhost:3000/admin/purchase-code");
+
+
+When("type in valid purchase code", async () => {
+    driver.findElement(webdriver.By.id("name")).sendKeys("20percentoff");
+	driver.findElement(webdriver.By.id("priceOff")).sendKeys("20");
+	await driver.sleep(3 * 1000);
+	
+});
+
+When("click the add button", async () => {
+    //driver.findElement(webdriver.By.id("name")).sendKeys(randomName);
+    //await driver.sleep(3 * 1000);
+	await driver.findElement(webdriver.By.xpath(`//*[@id="root"]/div/main/div/div/div/div[2]/div[1]/form/button`)).click();
+ 	await driver.sleep(6 * 1000);
+});
+
+Then("the purchase code should be added in", async () => {
+	let table = await driver.findElement(webdriver.By.className("ui table"));
+	let rows = await table.findElements(webdriver.By.tagName("tr"));
+	let cols = await (rows[rows.length-1]).findElements(webdriver.By.tagName("td"));
+	let item = await cols[1].getText();
+	let num = await cols[2].getText();
+	//let rows = await table.findElements(webdriver.By.tagName("tr"));
+	//let cols = await rows.findElements(webdriver.By.tagName("td")).getText();
+
+	//let item = await driver.findElement(By.xpath(`//*[@id="root"]/div/main/div/div/div/div[2]/table/tbody/tr[6]/td[2]`)).getText();
+	expect(item).to.contains("20percentoff");
+	expect(num).to.contains("20");
+	//delete: 
+	//const row = getByRole('cell', {name: 'Jane'}).closest('tr')
+	//fireEvent.click(
+	//within(row).getByText('Delete')
+	//)
 	//console.log(curURL);
     //expect(curURL).to.equal("http://localhost:3000/");
 });
 
-//---------------------------------------------------
-//Scenario: Accessing purchase code not successful
-//    Given user is logged in and at home page
-//    Then the user should not see admin button
 
-Given("user is logged in and at home page", async () => {
-	await driver.get("http://localhost:3000/");
-	await driver.sleep(3 * 1000);
-	//setTimeout(myFunction, 10000);
+/*
+Scenario: Adding purchase code correctly
+    Given user is logged in as admin 
+    When go to purchase code page url
+	When click the delete button at the end of row
+    Then the purchase code should be deleted
+
+
+When("click the delete button at the end of row", async () => {
+    //driver.findElement(webdriver.By.id("name")).sendKeys(randomName);
+    //await driver.sleep(3 * 1000);
+	let table = await driver.findElement(webdriver.By.className("ui table"));
+	let rows = await table.findElements(webdriver.By.tagName("tr"));
+	let cols = await (rows[rows.length-1]).findElements(webdriver.By.tagName("td"));
+	let item = await cols[3];
+
+	await item.click();
+ 	await driver.sleep(6 * 1000);
 });
 
-Then("the user should not see admin button", async () => {
-	//await driver.expect(Selector('#admin').exists).notOk(); 
-	await driver.expect(this.admin.exists).notOk()
-	//await driver.findElement(webdriver.By.xpath(`//*[@id="root"]/div/main/div/form/button`)) <= 0;
-	//await driver.sleep(6 * 1000);
+Then("the purchase code should be deleted", async () => {
+	let table = await driver.findElement(webdriver.By.className("ui table"));
+	let rows = await table.findElements(webdriver.By.tagName("tr"));
+	let cols = await (rows[rows.length-1]).findElements(webdriver.By.tagName("td"));
+	let item = await cols[3];
+	//let rows = await table.findElements(webdriver.By.tagName("tr"));
+	//let cols = await rows.findElements(webdriver.By.tagName("td")).getText();
+
+	//let item = await driver.findElement(By.xpath(`//*[@id="root"]/div/main/div/div/div/div[2]/table/tbody/tr[6]/td[2]`)).getText();
+	expect(item).to.contains("20percentoff");
+
+	//delete: 
+	//const row = getByRole('cell', {name: 'Jane'}).closest('tr')
+	//fireEvent.click(
+	//within(row).getByText('Delete')
+	//)
+	//console.log(curURL);
+    //expect(curURL).to.equal("http://localhost:3000/");
 });
+*/
