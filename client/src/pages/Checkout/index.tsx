859c6e8b49e8be7970cbd8dc9a3379a147f66dd1
GHysmu <yus60401@gmail.com>
2023-04-05 20:27:26 -0500

    apply purchase code

--- client/src/pages/Checkout/index.tsx
+++ client/src/pages/Checkout/index.tsx
@@ -1,24 +1,113 @@
-import React, { useContext } from "react";
-import { Table, Container, Header, Button,Input, Icon } from "semantic-ui-react";
+import React, {useContext,useRef, useEffect, useState, useCallback, useReducer,Reducer} from "react";
+import { Table, Container, Header, Button,Input, Icon, Item, Message , Form} from "semantic-ui-react";
 import { formatter } from "../../utils";
 import { ApplicationContext } from "../../context";
 import PayPalSmartPaymentButtons from "./PayPalSmartPaymentButtons";
 import { title } from "process";
+import {getCurrentItem, checkPurchaseCode} from "../../api/checkout"
 
-const Checkout: React.FC = (): JSX.Element => {
+interface Item {
+    readonly item_id: number;
+    name: string;
+	length: number;
+	price: number;
+}
+
+const Checkout: React.FC = (props): JSX.Element => {
 	const ctx = useContext(ApplicationContext);
+	//item
+	const item_id = 5;
+    const [ItemData, setItemData] = useState<Item[]>([]);
 
-	const total = formatter.format(
-		ctx.cart.map((value) => value.cost).reduce((accum, value) => accum + value, 0) / 100
-	);
+    useEffect(() => {
+		console.log("client try to get item data");
+            getCurrentItem(item_id)
+            .then(res => {
+                setItemData([res.data]);
+            })
+            .catch(error => console.error(error));
+    }, []);
 
+	//for purchase code
+	const [currentCode, setcurrentCode] = useState('');
+	const [totalPrice, settotalPrice] = useState(0);
+
+	const updateCode = (event: React.FormEvent<HTMLInputElement>) =>{
+		setcurrentCode((event.target as HTMLInputElement).value)
+	}
+	const hadnleApply = () => {
+		formStateDispatch({ type: "LOADING" });
+		console.log("current input");
+		console.log(currentCode);
+		checkPurchaseCode(currentCode).then(
+			async (res) => {
+				//console.log("discount");
+				//console.log(res.data);
+				const discounted = res.data * 0.01 * ItemData[0].price;
+				settotalPrice(discounted)
+				formStateDispatch({ type: "SUCCESS" })
+				})
+		.catch((err)=>{
+			//console.log("err")
+			//console.log(err);
+			formStateDispatch({
+				type: "REQUEST_ERROR",
+				payload:"Unable to apply. The account has already been created."
+			})
+		})
+	//}
+	
+};
+
+//form 
+const formStateReducer = (state: FormActionState, action: FormAction): FormActionState => {
+	switch (action.type) {
+		case "LOADING":
+			return {
+				loading: (action.payload as boolean) ?? true,
+				success: false,
+				requestError: undefined
+			};
+		case "SUCCESS":
+			return {
+				loading: false,
+				success: (action.payload as boolean) ?? true,
+				requestError: undefined
+			};
+		case "REQUEST_ERROR":
+			return {
+				loading: false,
+				success: false,
+				requestError: (action.payload as string)
+			}
+		default:
+			throw new Error(`Unknown action: ${action.type}`);
+	}
+};
+
+type FormAction = {
+	type: "LOADING" | "SUCCESS" | "REQUEST_ERROR";
+	payload?: boolean | string;
+};
+
+type FormActionState = {
+	loading: boolean;
+	requestError?: string;
+	success: boolean;
+	error?:string;
+};
+const [formState, formStateDispatch] = useReducer<Reducer<FormActionState, FormAction>>(
+	formStateReducer,
+	{ loading: false, success: false }
+);
+
+	//const ItemData1 = [{item_id: 1, name: 'Calculus 1(5 months)', length: 0, price: 20}]
 	return (
 		<Container>
 			<Table>
 				<Table.Header>
 					<Table.Row>
-						<Table.HeaderCell width={2}>Product</Table.HeaderCell>
-						<Table.HeaderCell width={2}>Publisher</Table.HeaderCell>
+						<Table.HeaderCell width={4}>Product</Table.HeaderCell>
 						<Table.HeaderCell collapsing width={2}>Length (days)</Table.HeaderCell>
 						<Table.HeaderCell collapsing width={2}>Original Price (USD)</Table.HeaderCell>
 						<Table.HeaderCell collapsing width={2}>Purchase Code</Table.HeaderCell>
@@ -26,26 +115,27 @@ const Checkout: React.FC = (): JSX.Element => {
 					</Table.Row>
 				</Table.Header>
 				<Table.Body>
-					<Table.Row >
-						<Table.Cell>Calculus 1</Table.Cell>
-						<Table.Cell>name</Table.Cell>
-						<Table.Cell>5 months</Table.Cell>
-						<Table.Cell>20</Table.Cell>
-						<Table.Cell>
-							<Input type="text" placeholder=""></Input>
+				{ItemData.map(item => (
+                                    <Table.Row key={item.item_id}>
+                                        <Table.Cell>{item.name}</Table.Cell>
+                                        <Table.Cell>{item.length}</Table.Cell>
+                                        <Table.Cell>{item.price}</Table.Cell>
+										<Table.Cell>
+						<Input type="text" name = "purchasecode" onChange={updateCode} value = {currentCode}></Input>
 						</Table.Cell>
 						<Table.Cell>
-						<button className="positive ui button">Apply</button>
+						<button className="positive ui button" onClick={hadnleApply} >Apply</button>
 						</Table.Cell>
-					</Table.Row>
+                                    </Table.Row>
+                                ))}
 				</Table.Body>
 				<Table.Footer>
 					<Table.Row>
-						<Table.HeaderCell colSpan={3}>
+						<Table.HeaderCell colSpan={2}>
 							<b>Total</b>
 						</Table.HeaderCell>
 						<Table.HeaderCell colSpan={2} collapsing>
-							<b>{total}</b>
+							<b>{totalPrice}</b>
 						</Table.HeaderCell>
 						<Table.HeaderCell>
 						<button className="positive ui button">
@@ -57,11 +147,31 @@ const Checkout: React.FC = (): JSX.Element => {
 					</Table.Row>
 				</Table.Footer>
 			</Table>
-			{ctx.user === undefined ? (
+
+			<Form
+				error={formState.requestError !== undefined}
+				loading={formState.loading}
+				success={formState.success}
+			>
+
+			<Message
+					content="You have successfully applied current purchase code"
+					header="SUCCESS"
+					success	
+			/>
+
+			<Message
+					content="Current purchase code is invalid"
+					header="ERROR"
+					error	
+			/>
+
+			</Form>
+			{/* {ctx.user === undefined ? (
 				<Header as="h3">You must be signed in to complete your purchase.</Header>
 			) : (
 				<PayPalSmartPaymentButtons total={total} />
-			)}
+			)} */}
 		</Container>
 	);
 };
