3f9b5608353e348a00d0f0ebe29613cf12460adb
lyq <root@lyq.me:wq>
2023-04-12 00:33:52 -0500

    Update Payment

---
+++ client/src/pages/Checkout/PayPalButtons.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+import React, { useEffect, useState, useContext } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { formatter } from "../../utils";
+import { ApplicationContext } from "../../context";
+
+type PayPalButtonsProps = {
+	total: string;
+};
+
+const PayPalButtons: React.FC<PayPalButtonsProps> = (
+	props
+): JSX.Element => {
+	
+	useEffect(() => {
+		const script = document.createElement('script');
+		
+		// Replace the client-id with your own sandbox Business account app client ID 
+		script.src = "https://www.paypal.com/sdk/js?client-id=AWuJ4TbTs8TF4PCyNsC3nZo-gJNpUTvebNbns0AvJWuAirsC3BRoTs4lW4_okNlpb0OQNtSZmada8Qtm&currency=USD";
+		script.async = true;
+
+		document.body.appendChild(script);
+
+		// @ts-ignore
+		paypal.Buttons({
+			// style: {
+			//   layout: "horizontal"
+			// },
+			// Sets up the transaction when a payment button is clicked
+			createOrder: function () {
+				return fetch("/my-server/create-paypal-order", {
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				// use the "body" param to optionally pass additional order information
+				// like product skus and quantities
+				body: JSON.stringify({
+					cart: [
+					{
+						sku: "YOUR_PRODUCT_STOCK_KEEPING_UNIT",
+						quantity: "YOUR_PRODUCT_QUANTITY",
+					},
+					],
+				}),
+				})
+				.then((response) => response.json())
+				.then((order) => order.id);
+			},
+			// Finalize the transaction after payer approval
+			// @ts-ignore
+			onApprove: function (data) {
+				return fetch("/my-server/capture-paypal-order", {
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					orderID: data.orderID,
+				}),
+				})
+				.then((response) => response.json())
+				.then((orderData) => {
+					// Successful capture! For dev/demo purposes:
+					console.log(
+					"Capture result",
+					orderData,
+					JSON.stringify(orderData, null, 2)
+					);
+					const transaction = orderData.purchase_units[0].payments.captures[0];
+					alert(
+					"Transaction " +
+						transaction.status +
+						": " +
+						transaction.id +
+						"\n\nSee console for all available details"
+					);
+					// When ready to go live, remove the alert and show a success message within this page. For example:
+					// var element = document.getElementById('paypal-button-container');
+					// element.innerHTML = '<h3>Thank you for your payment!</h3>';
+					// Or go to another URL:  actions.redirect('thank_you.html');
+				});
+			},
+		})
+		.render("#paypal-button-container");
+	}, []);
+
+	return <div id="paypal-button-container"></div>;
+};
+
+export default PayPalButtons;
