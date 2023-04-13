5d20cac34b8dfd53c34cf32b05db9daeb0b78752
Yongqing Liang at gvc-3090 <root@lyq.me>
2023-04-13 18:14:29 -0500

    Add the Pyapal payment.

--- server/src/payment/payment.controller.ts
+++ server/src/payment/payment.controller.ts
@@ -1,5 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { Request, Response } from 'express';
+
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+
+    constructor(private readonly paymentService: PaymentService) {}
+
+    @Post('create-paypal-order')
+    async create(@Req() req: Request, @Res() res: Response) {
+        this.paymentService.create(req, res);
+    }
+
+    @Post('capture-paypal-order')
+    async capture(@Req() req: Request, @Res() res: Response) {
+        this.paymentService.capture(req, res);
+    }
+}
 
--- server/src/payment/payment.module.ts
+++ server/src/payment/payment.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PaymentController } from './payment.controller';
+import { PaymentService } from './payment.service';
 
-@Module({})
+@Module({
+    controllers: [PaymentController],
+    providers: [PaymentService]
+})
 export class PaymentModule {}
 
