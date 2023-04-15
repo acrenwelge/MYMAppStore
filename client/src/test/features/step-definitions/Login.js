566a9dddbacb108b9459351472fdb0fcc951b61f
shuyuwang21 <112589309+shuyuwang21@users.noreply.github.com>
2023-04-14 22:10:00 -0500

    email confirm

--- client/src/test/features/step-definitions/Login.js
+++ client/src/test/features/step-definitions/Login.js
@@ -66,13 +66,113 @@ Then("unsuccessful login due to an invalid email.", async () => {
 	expect(errorMsg).to.equal("Incorrect email or password. Please ensure your email and password are correct and try again.");
 });
 
+
+
+
+
+//-----user confirmation-----
+
+
 /*Scenario4: Unsuccessful Login with unconfirmed email
-Given user navigates to the website to login
+Given user sign up and navigates to the website to login without confirming
 When I type 'dddddddd@me.com' and 'yushuang' as email and password
 And I click on 'login'
 Then unsuccessful login due to an email address unconfirmed.*/
 
-Then("unsuccessful login due to an email address unconfirmed.", async () => {
+function generateRandomString(length) {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+const randomName = generateRandomString(10); // generates a random string of length 10
+const randomEmail = randomName + '@me.com'
+Given("user sign up and navigates to the website to login without confirming", async () => {
+    //sign up:
+    await driver.get("http://localhost:3000/sign-up");
+    await driver.sleep(3 * 1000);
+
+    driver.findElement(webdriver.By.id("name")).sendKeys(randomName);
+    driver.findElement(webdriver.By.id("email")).sendKeys(randomEmail);
+    driver.findElement(webdriver.By.id("confirmEmail")).sendKeys(randomEmail);
+    driver.findElement(webdriver.By.id("password")).sendKeys("123");
+    driver.findElement(webdriver.By.id("confirmPassword")).sendKeys("123");
+    await driver.sleep(6 * 1000);
+
+    await driver.findElement(webdriver.By.className("ui green active fluid button")).click();
+    await driver.sleep(6 * 1000);
+
+    await driver.get("http://localhost:3000/login");
+    await driver.sleep(3 * 1000);
+    //setTimeout(myFunction, 10000);
+});
+
+When("I type in email and password",  async () => {
+//When("the user enters their email and password", async () => {
+    driver.findElement(webdriver.By.id("email")).sendKeys(randomEmail);
+    driver.findElement(webdriver.By.id("password")).sendKeys("123");
+    await driver.sleep(6 * 1000);
+});
+
+Then("unsuccessful login due to an email address unconfirmed", async () => {
     let errorMsg = await driver.findElement(webdriver.By.className("ui error message")).getText();
     expect(errorMsg).to.equal("Account is not activated. Please check your email to confirm your account.");
 });
+
+
+
+
+
+//Scenario 5: Successful activation
+
+//Scenario: Successful activation
+//    Given user sign up
+//    When user go to the activate url
+//    Then account is activated in activate link
+
+Given("user sign up", async () => {
+    //sign up:
+    await driver.get("http://localhost:3000/sign-up");
+    await driver.sleep(3 * 1000);
+
+    driver.findElement(webdriver.By.id("name")).sendKeys(randomName);
+    driver.findElement(webdriver.By.id("email")).sendKeys(randomEmail);
+    driver.findElement(webdriver.By.id("confirmEmail")).sendKeys(randomEmail);
+    driver.findElement(webdriver.By.id("password")).sendKeys("123");
+    driver.findElement(webdriver.By.id("confirmPassword")).sendKeys("123");
+    await driver.sleep(6 * 1000);
+
+    await driver.findElement(webdriver.By.className("ui green active fluid button")).click();
+    await driver.sleep(6 * 1000);
+
+    //setTimeout(myFunction, 10000);
+});
+When("user go to the activate url",  async () => {
+    //confirm:
+    await driver.get("http://localhost:3000/activate?activationCode=naomi2049"+randomName+"114514");
+    await driver.sleep(6 * 1000);
+});
+Then("account is activated in activate link", async () => {
+    let successMsg = await driver.findElement(webdriver.By.className("ui error message")).getText();
+    expect(successMsg).to.equal("Account is activated.");
+});
+
+
+
+
+
+
+/*
+Scenario 6: Unsuccessful activation due to wrong url
+    Given user sign up
+    When user go to wrong activate url
+    Then account is not activated in activate link
+
+Scenario 7: Unsuccessful activation due to used url
+    Given user sign up
+    When user go to a used activate url
+    Then account is not activated in activate link
+*/
