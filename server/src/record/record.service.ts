9e6649018ca809e0256e3e6c7c25482d748064fb
Zhiting Zhao <helenzhao@tamu.edu>
2023-04-21 13:53:39 -0500

    update record

--- server/src/record/record.service.ts
+++ server/src/record/record.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/item/entities/item.entity';
 import { Repository } from 'typeorm';
 import { CreateRecordDto } from './dto/create-record.dto';
 import { UpdateRecordDto } from './dto/update-record.dto';
@@ -37,19 +38,29 @@ export class RecordService {
     return `This action returns a #${id} record`;
   }
 
-  async update(user_id: number, expirationDate: Date, item_id: number) {
+  async update(user_id: number, item_id: number) {
     const records = await this.recordRepo.find({
       where: {
         user_id:user_id,
         item_id:item_id
       }
     })
+    // find length from item using item_id
+    const length = 5; 
+    // const item_info = Item.findOne(item_id);
+    // console.log(item_info);
     if (records == null){//add a new record
+      const newDate =  new Date(Date.now()+length*30);
       const newrecord = new Record();
-      
+      newrecord.item_id = item_id;
+      newrecord.expirationDate = newDate;
+      newrecord.user_id = user_id;
+      console.log(newrecord);
+      const record = await this.recordRepo.save(newrecord);
+      return record;
     }
     else{//update old record
-
+      
     }
     //return `This action updates a #${id} record`;
   }
