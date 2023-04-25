1e0a9f60b70bc19ebcdccf1217d92bedf18206cd
Ncc <luminousncc@gmail.com>
2023-04-25 01:07:34 -0500

    update Heroku PORT

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
 
-  await app.listen(configService.get<string>('SERVER_PORT'),()=>{
+  await app.listen(process.env.PORT || configService.get<string>('SERVER_PORT'),()=>{
     logger.log('Ready Listen' + configService.get<string>('SERVER_PORT'))
     logger.log('MyMathApp Server Application started'); // log a message using the logger instance
     logger.log('MyMathApp Server running at ' + configService.get<string>('ENV_TYPE') + ' Mode')
