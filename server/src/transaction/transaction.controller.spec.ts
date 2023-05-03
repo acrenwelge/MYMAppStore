5bd426364d4a97e063b12ce14ca6324d05ecf2bc
lyq <root@lyq.me:wq>
2023-05-03 12:29:50 -0500

    Update tmp

--- server/src/transaction/transaction.controller.spec.ts
+++ server/src/transaction/transaction.controller.spec.ts
@@ -8,29 +8,42 @@ const moduleMocker = new ModuleMocker(global);
 
 describe('TransactionController', () => {
   let controller: TransactionController;
+  let provider: TransactionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransactionController],
-      // providers: [TransactionService],
-    }).useMocker((token) => {
-      const results = ['test1', 'test2'];
-      if (token == TransactionService) {
-        return {
-          findAll: jest.fn().mockResolvedValue(results)
-        };
-      }
-      if (typeof token == 'function') {
-        const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
-        const Mock = moduleMocker.generateFromMetadata(mockMetadata);
-        return new Mock();
-      }
+      providers: [TransactionService],
+    // }).useMocker((token) => {
+    //   const results = ['test1', 'test2'];
+    //   console.log('token', token);
+    //   if (token == TransactionService) {
+    //     return {
+    //       findAll: jest.fn().mockResolvedValue(results)
+    //     };
+    //   }
+    //   if (typeof token == 'function') {
+    //     const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+    //     const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+    //     return new Mock();
+    //   }
     }).compile();
 
     controller = module.get<TransactionController>(TransactionController);
+    provider = module.get<TransactionService>(TransactionService);
+    
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  // describe('findAll', () => {
+  //   it('should return an array of cats', async () => {
+  //     const result = ['test1', 'test2'];
+  //     jest.spyOn(provider, 'findAll').mockImplementation(() => result);
+
+  //     expect(await controller.findAll()).toBe(result);
+  //   });
+  // });
 });
