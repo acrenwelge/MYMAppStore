aab290a0fe388d0d41e40c6964f81451af826b60
lyq <root@lyq.me:wq>
2023-05-03 12:12:19 -0500

    Add a basic test and coverage.

--- server/src/transaction/transaction.controller.spec.ts
+++ server/src/transaction/transaction.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
+
 
 describe('TransactionController', () => {
   let controller: TransactionController;
@@ -8,7 +12,19 @@ describe('TransactionController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransactionController],
-      providers: [TransactionService],
+      // providers: [TransactionService],
+    }).useMocker((token) => {
+      const results = ['test1', 'test2'];
+      if (token == TransactionService) {
+        return {
+          findAll: jest.fn().mockResolvedValue(results)
+        };
+      }
+      if (typeof token == 'function') {
+        const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+        const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+        return new Mock();
+      }
     }).compile();
 
     controller = module.get<TransactionController>(TransactionController);
