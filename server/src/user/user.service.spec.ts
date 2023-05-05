d7b0e019acc9729edac2720883dfbcf05e85d2c7
shuyuwang21 <112589309+shuyuwang21@users.noreply.github.com>
2023-05-04 20:31:16 -0500

    user test

--- server/src/user/user.service.spec.ts
+++ server/src/user/user.service.spec.ts
@@ -7,23 +7,38 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { createMock } from '@golevelup/ts-jest';
 import {EmailService} from "../email/email.service";
+import {CreateTransactionDto} from "../transaction/dto/create-transaction.dto";
 
 describe('UserService', () => {
   let service: UserService;
+  let emailservice: EmailService;
   let repositoryMock: MockType<Repository<User>>;
+
+  const UserServiceMock =
+      { localSignUp: jest.fn(),
+        generateActivationCode: jest.fn(),
+        authenticate: jest.fn(),
+        activateAccount: jest.fn(),
+        findOne: jest.fn(),
+        findAll: jest.fn(),
+          find: jest.fn(),
+          save: jest.fn()
+      };
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
           UserService,
+          EmailService,
           {
             provide: getRepositoryToken(User),
-            useValue: createMock<User>(),
+            useValue: UserServiceMock,
           }
     ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    emailservice = module.get<EmailService>(EmailService);
     repositoryMock = module.get(getRepositoryToken(User));
   });
 
@@ -35,18 +50,49 @@ describe('UserService', () => {
     expect(service).toBeDefined();
   });
 
+    it('should signup locally', async () => {
+        const olduser = null;
+        const testuser = new User();
+        jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => olduser);
+        jest.spyOn(repositoryMock, 'save').mockImplementation(() => testuser);
+        expect(await service.localSignUp(testuser)).toBe(true);
+    });
+
+  it('should generate a new activation code', () => {
+      const oldstr = "abc";
+      //const newstr = 'Naomi2049'+ Date.now().toString()+'ncclovekk';
+      //@ts-expect-error
+      jest.spyOn(repositoryMock, 'generateActivationCode').mockImplementation(() => newstr);
+      expect(service.generateActivationCode(oldstr)).toStrictEqual('Naomi2049'+ Date.now().toString()+'ncclovekk');
+  });
+  //if too slow it won't work, due to
+
+
+    it('should activate an account', async () => {
+      const activcode = "a";
+      const olduser = new User();
+      olduser.activationCode = activcode;
+      olduser.activatedAccount = false;
+      const newuser = new User();
+      newuser.activationCode = activcode;
+      newuser.activatedAccount = true;
+      jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => olduser);
+      jest.spyOn(repositoryMock, 'save').mockImplementation(() => newuser);
+      expect(await service.activateAccount(newuser.activationCode)).toStrictEqual(newuser);
+  });
+
+  it('should find all user', async () => {
+
+      jest.spyOn(repositoryMock, 'find').mockImplementation(() => true);
+      expect(await service.findAll()).toBe(true);
+  });
+
   it('should find one user', async () => {
     const result = new User();
     result.email = "ncc@me.com";
     jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => result);
     expect(await service.findOne("ncc@me.com")).toBe(result);
   });
-
-  it('should find all user', async () => {
-    jest.spyOn(repositoryMock, 'find').mockImplementation(() => true);
-    expect(await service.findAll()).toBe(true);
-  });
-
 
 });
 
