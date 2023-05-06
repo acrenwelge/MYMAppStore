c0bcc01992d7e047a7f98156f61268fad3098ff8
Ncc <luminousncc@gmail.com>
2023-05-05 19:08:38 -0500

    Exclude strategy|guard|enum|decorator in test coverage

--- server/src/record/record.controller.ts
+++ server/src/record/record.controller.ts
@@ -1,7 +1,6 @@
 import {Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, HttpCode} from '@nestjs/common';
 import { RecordService } from './record.service';
 import { CreateRecordDto } from './dto/create-record.dto';
-import { UpdateRecordDto } from './dto/update-record.dto';
 import { User } from 'src/user/entities/user.entity';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { Record } from './entities/record.entity';
