16bb0967479c7a29d6602c5bd35bd497da846738
smbanasik <smbanasik@gmail.com>
2023-09-28 17:27:37 -0500

    Got all but one unit test case working. I am unsure how to handle promise rejection at this time.

--- server/src/user/user.service.spec.ts
+++ server/src/user/user.service.spec.ts
@@ -9,150 +9,159 @@ import {EmailService} from "../email/email.service";
 import { hash } from 'bcrypt';
 import { SubscriptionService } from 'src/subscription/subscription.service';
 import { UserDto } from './user.dto';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('UserService', () => {
-  let service: UserService;
-  let emailservice: EmailService;
-  let repositoryMock: MockType<Repository<UserEntity>>;
+	let service: UserService;
+	let emailservice: EmailService;
+	let repositoryMock: MockType<Repository<UserEntity>>;
 
-  // const UserServiceMock =
-  //     { localSignUp: jest.fn(),
-  //       generateActivationCode: jest.fn(),
-  //       authenticate: jest.fn(),
-  //       activateAccount: jest.fn(),
-  //       findOne: jest.fn(),
-  //       findAll: jest.fn(),
-  //         find: jest.fn(),
-  //         save: jest.fn()
-  //     };
+	// const UserServiceMock =
+	//     { localSignUp: jest.fn(),
+	//       generateActivationCode: jest.fn(),
+	//       authenticate: jest.fn(),
+	//       activateAccount: jest.fn(),
+	//       findOne: jest.fn(),
+	//       findAll: jest.fn(),
+	//         find: jest.fn(),
+	//         save: jest.fn()
+	//     };
 
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-          UserService,
-          {
-            provide: SubscriptionService,
-            useValue: createMock<SubscriptionService>(),
-          },
-          {
-            provide: EmailService,
-            useValue: createMock<EmailService>(),
-          },
-          {
-            provide: getRepositoryToken(UserEntity),
-            useValue: createMock<UserEntity>(),
-          }
-    ],
-    }).compile();
-    service = module.get<UserService>(UserService);
-    emailservice = module.get<EmailService>(EmailService);
-    repositoryMock = module.get(getRepositoryToken(UserEntity));
-  });
+	beforeAll(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+					UserService,
+					{
+						provide: SubscriptionService,
+						useValue: createMock<SubscriptionService>(),
+					},
+					{
+						provide: EmailService,
+						useValue: createMock<EmailService>(),
+					},
+					{
+						provide: getRepositoryToken(UserEntity),
+						useValue: createMock<UserEntity>(),
+					}
+		],
+		}).compile();
+		service = module.get<UserService>(UserService);
+		emailservice = module.get<EmailService>(EmailService);
+		repositoryMock = module.get(getRepositoryToken(UserEntity));
+	});
 
-  afterAll(() => {
-    jest.clearAllMocks();
-  });
+	afterAll(() => {
+		jest.clearAllMocks();
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 
-  it('should signup locally', async () => {
-      const testUser = new UserEntity();
-      testUser.email = "testing@mail.com"
-      const userClient = new UserDto();
-      userClient.email = "testing@mail.com"
-      jest.spyOn(repositoryMock, 'exist').mockImplementation(() => false);
-      jest.spyOn(repositoryMock, 'save').mockImplementation(() => testUser);
-      jest.spyOn(emailservice, 'sendActivateAccountEmail').mockReturnValue(Promise.resolve());
-      expect((await service.localSignUp(userClient)).email).toBe(testUser.email);
-  });
+	it('should signup locally', async () => {
+			const testUser = new UserEntity();
+			testUser.email = "testing@mail.com"
+			const userClient = new UserDto();
+			userClient.email = "testing@mail.com"
+			jest.spyOn(repositoryMock, 'exist').mockImplementation(() => false);
+			jest.spyOn(repositoryMock, 'save').mockImplementation(() => testUser);
+			jest.spyOn(emailservice, 'sendActivateAccountEmail').mockReturnValue(Promise.resolve());
+			expect((await service.localSignUp(userClient)).email).toBe(testUser.email);
+	});
 
-  it('should activate an account', async () => {
-    const activcode = "a";
-    const olduser = new UserEntity();
-    olduser.activationCode = activcode;
-    olduser.activatedAccount = false;
-    const newuser = new UserEntity();
-    newuser.activationCode = activcode;
-    newuser.activatedAccount = true;
-    jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => olduser);
-    jest.spyOn(repositoryMock, 'save').mockImplementation(() => newuser);
-    expect(await service.activateAccount(newuser.activationCode)).toStrictEqual(newuser);
-  });
+	it('should activate an account', async () => {
+		const activcode = "a";
+		const olduser = new UserEntity();
+		olduser.activationCode = activcode;
+		olduser.activatedAccount = false;
+		const newuser = new UserEntity();
+		newuser.activationCode = activcode;
+		newuser.activatedAccount = true;
+		jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => olduser);
+		jest.spyOn(repositoryMock, 'save').mockImplementation(() => newuser);
+		expect(await service.activateAccount(newuser.activationCode)).toStrictEqual(newuser);
+	});
 
-  it('should find all user', async () => {
-    const userList = []
-    const user = new UserEntity();
-    user.email = "banana@gmail.com"
-    userList.push(user)
-    user.email = "temporary@mail.com"
-    userList.push(user)
-    jest.spyOn(repositoryMock, 'find').mockImplementation(() => userList);
-    expect((await service.findAll()).pop().email).toBe(userList[1].email);
-  });
+	it('should find all user', async () => {
+		const userList = []
+		const user = new UserEntity();
+		user.email = "banana@gmail.com"
+		userList.push(user)
+		user.email = "temporary@mail.com"
+		userList.push(user)
+		jest.spyOn(repositoryMock, 'find').mockImplementation(() => userList);
+		expect((await service.findAll()).pop().email).toBe(userList[1].email);
+	});
 
-  it('should find one user', async () => {
-    const result = new UserEntity();
-    result.email = "ncc@me.com";
-    jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => result);
-    expect(await service.findOneByEmail("ncc@me.com")).toBe(result);
-  });
+	it('should find one user', async () => {
+		const result = new UserEntity();
+		result.email = "ncc@me.com";
+		jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => result);
+		expect(await service.findOneByEmail("ncc@me.com")).toBe(result);
+	});
 
-  it('should hash a password', async () => {
-    const plaintext = "123";
-    const res = await hash(plaintext, 10)
-    console.log(res)
-    expect(res).toHaveLength(60);
-  })
+	it('should hash a password', async () => {
+		const plaintext = "123";
+		const res = await hash(plaintext, 10)
+		console.log(res)
+		expect(res).toHaveLength(60);
+	})
 
-  // TODO: Does not work
-  it('should authenticate a user', async () => {
-    const result = new UserEntity();
-    result.email = "test@mail.com";
-    result.passwordHash = "abcdefg";
-    jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => result);
-    expect(await service.authenticate(result)).toBe(result);
-  });
+	it('should authenticate a user', async () => {
+		const password = "abcdefg"
+		const result = new UserDto()
+		result.email = "test@mail.com"
+		result.password = password
+		const foundUser = new UserEntity()
+		foundUser.email = "test@mail.com"
+		foundUser.passwordHash = await hash(password,10)
+		jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => foundUser)
+		expect(await service.authenticate(result)).toBe(foundUser)
+	});
 
-  // TODO: Does not work
-  it('should NOT authenticate a user', async () => {
-    const result = new UserEntity();
-    result.email = "test@mail.com";
-    result.passwordHash = "abcdefg";
-    jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => null);
-    expect(await service.authenticate(result)).toBeNull();
-  });
+	// TODO: Does not work
+	// I need to figure out how to handle promise rejection.
+	xit('should NOT authenticate a user', async () => {
+		const password = "abcdefg"
+		const result = new UserDto()
+		result.email = "test@mail.com"
+		result.password = password
+		const foundUser = new UserEntity()
+		foundUser.email = "test@mail.com"
+		foundUser.passwordHash = await hash("aaaaaaa",10)
+		jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => foundUser)
+		await expect(await service.authenticate(result)).rejects.toThrow("Incorrect email or password")
+	});
 
-  it('should delete a user', async () => {
-    jest.spyOn(repositoryMock, 'delete').mockImplementation(() => null);
-    await service.deleteOne(1);
-    expect(repositoryMock.delete).toBeCalledWith(1);
-  });
+	it('should delete a user', async () => {
+		jest.spyOn(repositoryMock, 'delete').mockImplementation(() => null);
+		await service.deleteOne(1);
+		expect(repositoryMock.delete).toBeCalledWith(1);
+	});
 
-  it('should change first and last name', async () => {
-    const oldUser = new UserEntity()
-    oldUser.email = "test@mail.com"
-    oldUser.firstName = "Testing"
-    oldUser.lastName = "Bad name"
-    const user = new UserDto()
-    user.email = "test@mail.com"
-    user.firstName = "John"
-    user.lastName = "Doe"
-    jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => oldUser);
-    expect((await service.updateOne(user)).firstName).toBe(user.firstName)
-  });
+	it('should change first and last name', async () => {
+		const oldUser = new UserEntity()
+		oldUser.email = "test@mail.com"
+		oldUser.firstName = "Testing"
+		oldUser.lastName = "Bad name"
+		const user = new UserDto()
+		user.email = "test@mail.com"
+		user.firstName = "John"
+		user.lastName = "Doe"
+		jest.spyOn(repositoryMock, 'findOne').mockImplementation(() => oldUser);
+		expect((await service.updateOne(user)).firstName).toBe(user.firstName)
+	});
 
-  it('should change password', async () => {
-    const user = new UserEntity()
-    user.email = "test@mail.com"
-    const update = new UserDto()
-    update.email = "test@mail.com"
-    update.password = "applesButter32Game"
-    jest.spyOn(repositoryMock, 'update').mockImplementation(() => user)
-    // We have the same expectation that the password hash test does
-    expect((await service.updateOne(update)).passwordHash).toHaveLength(60)
-  });
+	it('should change password', async () => {
+		const user = new UserEntity()
+		user.email = "test@mail.com"
+		const update = new UserDto()
+		update.email = "test@mail.com"
+		update.password = "applesButter32Game"
+		jest.spyOn(repositoryMock, 'update').mockImplementation(() => user)
+		// We have the same expectation that the password hash test does
+		expect((await service.updateOne(update)).passwordHash).toHaveLength(60)
+	});
 
 });
 
