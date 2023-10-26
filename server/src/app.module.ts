26e6c0332ced8f8a10d1d9188f0ead8d08dacb19
nikhilnehra-tamu <143118666+nikhilnehra-tamu@users.noreply.github.com>
2023-10-25 19:13:58 -0500

    iter3 start

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -57,7 +57,7 @@ if (process.env.RUNNING_ENV === 'prod') {
 				UserEntity, ClassEntity, ItemEntity, PurchaseCodeEntity, SubscriptionEntity,
 				TransactionEntity, TransactionDetailEntity, FreeSubscriptionEntity
 			],
-			synchronize: process.env.ENV_TYPE.toUpperCase() === 'DEV',
+			synchronize: true,
 		}) : TypeOrmModule.forRoot({
 			type: 'postgres',
 			url: process.env.DATABASE_URL,
