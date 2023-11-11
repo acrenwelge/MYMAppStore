c39dbdf77694b2885df43c9f3fd143df76821caf
smbanasik <smbanasik@gmail.com>
2023-11-10 20:38:04 -0600

    quick fix

--- client/src/components/Header/NavMenu.tsx
+++ client/src/components/Header/NavMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Menu, Dropdown } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import ApplicationContext from "../../context/application.context";
